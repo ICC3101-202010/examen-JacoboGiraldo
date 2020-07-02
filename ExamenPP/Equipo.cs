a429ac71d5f6b4a957c6bd6d226fa8cf2a905f6b
Juan Jacobo Giraldo Rodriguez <juanjacobogiraldorodriguez@Juans-MacBook-Air-2.local>
2020-07-02 16:16:44 -0400

    Equipo Funcional, implementacion lack

--- ExamenPP/Equipo.cs
+++ ExamenPP/Equipo.cs
@@ -4,15 +4,19 @@ namespace ExamenPP
 {
     public class Equipo
     {
-        public Equipo()
+        public Equipo(string T, string ET, string N)
         {
+            tipo = T;
+            espTipo = ET;
+            nombre = N;
         }
 
-        private string tipo;
-        private string espTipo;
-        private string nombre;
+        private string tipo; // Nacional - Liga
+        private string espTipo; // Nombre de la nacion - Nombre de la liga
+        private string nombre; // Nombre del equipo
 
         private List<Jugadores> JugadoresEquipo;
+        
 
         public string Tipo { get => tipo; set => tipo = value; }
         public string EspTipo { get => espTipo; set => espTipo = value; }
@@ -20,6 +24,9 @@ namespace ExamenPP
 
         public void FullTeamInfo()
         {
+            Console.WriteLine("Nombre del equipo: "+nombre);
+            Console.WriteLine("Tipo de equipo: "+tipo+" -> "+espTipo);
+            Console.WriteLine("Plantel de Jugadores:");
             foreach (Jugadores JugadorRandom in JugadoresEquipo)
             {
                 Console.WriteLine(JugadorRandom.GetInfo());
@@ -28,18 +35,37 @@ namespace ExamenPP
 
         public void AgregarJugador(string No, int Ed, string Na, int Su, string Po, int PtA, int PtD, int NuC)
         {
-            Jugadores JX = new Jugadores();
-            JX.Nombre = No;
-            JX.Edad = Ed;
-            JX.Nacion = Na;
-            JX.Sueldo = Su;
-            JX.Posicion = Po;
-            JX.PtosAtaque1 = PtA;
-            JX.PtosDefensa1 = PtD;
-            JX.NumeroCamiseta1 = NuC;
-            JugadoresEquipo.Add(JX);
+            
+            if (tipo == "Nacional" && espTipo == Na)
+            {
+                Jugadores JX = new Jugadores();
+                JX.Nombre = No;
+                JX.Edad = Ed;
+                JX.Nacion = Na;
+                JX.Sueldo = Su;
+                JX.Posicion = Po;
+                JX.PtosAtaque1 = PtA;
+                JX.PtosDefensa1 = PtD;
+                JX.NumeroCamiseta1 = NuC;
+                JugadoresEquipo.Add(JX);
+            }
+            else if (tipo == "Nacional" && espTipo != Na)
+            {
+                Console.WriteLine("Si el equipo es nacional el pais del equipo debe ser igual a la nacionalidad del jugador. Intentelo de nuevo.");
+            }
+            else if (tipo == "Liga")
+            {
+                Jugadores JX = new Jugadores();
+                JX.Nombre = No;
+                JX.Edad = Ed;
+                JX.Nacion = Na;
+                JX.Sueldo = Su;
+                JX.Posicion = Po;
+                JX.PtosAtaque1 = PtA;
+                JX.PtosDefensa1 = PtD;
+                JX.NumeroCamiseta1 = NuC;
+                JugadoresEquipo.Add(JX);
+            }     
         }
-
-        
     }
 }
--- ExamenPP/Program.cs
+++ ExamenPP/Program.cs
@@ -6,11 +6,16 @@ namespace ExamenPP
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Ingrese el tipo de equipo (Liga/Nacional)");
+            string tipo = Console.ReadLine();
+            Console.WriteLine("Especifique el nombre de la liga o nacinalidad del equipo:");
+            string espTipo = Console.ReadLine();
+            Console.WriteLine("Inserte el nombre del equipo:");
+            string nombre = Console.ReadLine();
 
+            Equipo NewTeam = new Equipo(tipo, espTipo, nombre);
 
-            Equipo NewTeam = new Equipo();
-
-
+            
 
 
 
